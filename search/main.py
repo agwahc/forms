113cc40301d88e60986d7be00c91edf45b03814a
Agwah_Chigozirim <agwahchigozirim@gmail.com>
2021-05-18 03:36:08 +0100

    first stage complete

--- search/main.py
+++ search/main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to forms everywhere for classes, files, tool windows, actions, and settings.
+from pdfminer.high_level import extract_text
+import linecache
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press Ctrl+F8 to toggle the breakpoint.
+form = []
+
+def criterion():  # defines search requirements
+    a, b = input('enter search'), input('enter class')
+    finder(a, b)
 
 
-# Press the green button in the gutter to run the script.
+def pre():  # defines class for search
+    content = extract_text('AbrahamLincoln.pdf')
+    c = open('test.txt', 'w')
+    c.write(content.lower())
+    c.close()
+
+
+def period(x):  # locates periods
+    for i in x:
+        if i.find('.') != -1:
+            end = x.index(i) + 1
+            return end
+
+def compiler(x):
+    with open('sum.txt', 'w') as f:
+        content = ' '.join(x)
+        new = content.split('.')
+        print(new)
+        for i in new:
+            f.write(i + '\n')
+    f.close()
+
+def finder():
+    search = 'birth'
+    pre()
+    filter()
+    with open('test.txt', 'r') as file:
+        for num, i in enumerate(file, 1):
+            x = i.split()
+            for y in x:
+                if y.find(search) != -1:
+                    if period(i.split()) is not None:
+                        start, end = x.index(y), period(x)
+                        r = range(start, end)
+                        for v in r:
+                            form.append(x[v])
+                    else:
+                        start, end = x.index(y), len(x)
+                        r = range(start, end)
+                        for v in r:
+                            form.append(x[v])
+                        check(num)
+    compiler(form)
+
+def check(num):
+    x = linecache.getline('test.txt', num + 1)
+    y = x.split()
+    if period(y) is not None:
+        start, end = 0, period(y)
+        r = range(start, end)
+        for v in r:
+            form.append(y[v])
+    else:
+        form.append(x)
+
+
+
+def filter():
+    with open("test.txt") as f:
+        c = ("".join(line for line in f if not line.isspace()))
+    with open("test.txt", 'w') as e:
+        e.write(c)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    finder()
--- search/test.py
+++ search/test.py
@@ -1,4 +1,53 @@
+from wand.image import Image
+import os
+
 def organizer(): # creates a dict from words and meaning and appends to already created dict
     k = []
     m = []
     k1 = k.append(input('enter'))
+
+'''
+def finder(a, b):
+    access = PyPDF2.PdfFileReader(open('AbrahamLincoln.pdf', 'rb'))
+    pages = list(range(0, access.numPages))
+    print(access.numPages)
+    find = access.getPage(4)
+    content = find.extractText()
+
+
+pdf_file = "scanned_apple_10k_snippet.pdf"
+    files = []
+    with(Image(filename=pdf_file, resolution=500)) as conn:
+        for index, image in enumerate(conn.sequence):
+            image_name = os.path.splitext(pdf_file)[0] + str(index + 1) + '.png'
+            Image(image).save(filename=image_name)
+            files.append(image_name)
+            
+def finder():
+    content, form = extract_text('AbrahamLincoln.pdf'), []
+    c = open('test.txt', 'w')
+    c.write(content.lower())
+    c.close()
+    for i in open('test.txt', 'r'):
+        x = i.split()
+        for y in x:
+            if y == 'sought':
+                start, end, catch = x.index(y), len(x), period(x)
+                if catch:
+                    end = catch
+                else:
+                    pass
+                print(catch, end)
+                r = range(start, end)
+                for v in r:
+                    form.append(x[v])
+    print(form)
+
+s = 'he sought the holding for me. Plus my fam'
+b = s.split()
+for i in b:
+    if i.find('sought') != -1:
+        print(b.index(i))
+'''
+x = 'xmskc. kajnkvk jdsniunj sdjsiu'
+print(x.split('.'))
